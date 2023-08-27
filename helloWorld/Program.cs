67e3f44dddd65aecf433c08f51572b0b5ac411ef
Neyris Bohorquez <113575840+NeyrisBH@users.noreply.github.com>
2023-08-27 16:14:32 -0500

    Initial commit

---
+++ helloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Programar que calcula el área de un rectangulo.
+int ladoA;
+int ladoB;
+int resultado;
+
+Console.WriteLine("Calcula el área de un rectangulo");
+Console.WriteLine("Ingresa el lado del lado A: ");
+ladoA = Convert.ToInt32(Console.ReadLine());
+ladoB = Convert.ToInt32(Console.ReadLine());
+resultado = ladoA * ladoB;
+Console.WriteLine(resultado);
